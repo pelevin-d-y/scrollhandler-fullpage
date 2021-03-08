452e8eb5e31c88296110eca93f419b1c8954bbf3
Denis <pelevin.d.y@gmail.com>
2021-03-08 17:49:39 +0300

    first

--- baseClass/index.js
+++ baseClass/index.js
@@ -1,23 +1,62 @@
 class BaseClass {
-  constructor(baseContainer, section, time) {
-    this.section = section
+  constructor(baseContainer, sections, time) {
+    this.sections = Array.from(sections)
     this.baseContainer = baseContainer
     this.time = time
 
     this.currentSection = 0
+    this.currentTransform = 0
+
+
+    this.isScrollAllowed = {
+      m: { up: true, down: true, left: true, right: true },
+    }
+
+    this.prevTime = null
+    this.curTime = null
+    this.timeDiff = null
+    this.scrollings = []
+
+    this.canScroll = true
+    this.afterSectionLoadsId = null
+
+    this['next'] = () => {
+      this.setCurrentSection(this.currentSection + 1)
+      this.setCurrentTransform()
+      if (this.getNextTransformValue() >= this.getBaseContainerHeight) {
+        return
+      }
+      this.startTransition('next')
+      this.setActiveNavigation()
+    }
   }
 
-  get sectionHeight() {
-    return this.section.clientHeight
+  getSectionHeight() {
+    return this.sections[0].clientHeight
   }
 
-  get baseContainerHeight() {
+  getBaseContainerHeight() {
     return this.baseContainer.clientHeight
   }
 
-  set currentSection(value) {
+  getSectionsQuantity() {
+    return this.sections.length
+  }
+
+  setCurrentSection(value) {
     this.currentSection = value 
   }
+
+  setCurrentTransform() {
+    this.currentTransform = this.currentSection * this.getSectionHeight()
+  }
+
+  startTransition = (direction) => {
+
+
+    var translate3d = 'translate3d(0px, -' + this.currentTransform + 'px, 0px)'
+    this.css(this.baseContainer, this.getTransforms(translate3d))
+  }  
 }
 
 export default BaseClass
--- scripts.js
+++ scripts.js
@@ -2,12 +2,22 @@ import ScrollHandler from './scrollhandler.js'
 import BaseClass from './baseClass/index.js'
 
 window.addEventListener('load', () => {
-  console.log('BaseClass', BaseClass)
+  
+  // const baseContainerEl = document.querySelector('.base-container')
+  // const sectionEl = document.querySelector('.section')
+  // const scrollHandler = new ScrollHandler(
+  //   baseContainerEl,
+  //   sectionEl,
+  //   700
+  // )
+
   const baseContainerEl = document.querySelector('.base-container')
-  const sectionEl = document.querySelector('.section')
-  const scrollHandler = new ScrollHandler(
+  const sectionEl = document.querySelectorAll('.section')
+  const baseClass = new BaseClass(
     baseContainerEl,
     sectionEl,
     700
   )
+
+  console.log('baseClass.sections', baseClass.sections)
 })
