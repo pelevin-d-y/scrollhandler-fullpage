afba1a7a509ab53cde1eb4ead23f856b20160ae6
Denis <pelevin.d.y@gmail.com>
2021-03-09 17:42:54 +0300

    add index file to fullpage

---
+++ fullpage/index.js
@@ -0,0 +1,9 @@
+import Events from './Events.js'
+
+class FullPage extends Events {
+  constructor(baseContainer, sections, time) {
+    super(baseContainer, sections, time)
+  }
+}
+
+export default FullPage
